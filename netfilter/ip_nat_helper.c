72bcde1b35e5635e3c59407a84f61075cfad1c50
WenqingWu <wenqingwu2015@gmail.com>
2018-05-24 10:47:25 +0800

    modified headers netfilter/ip_nat_helper.c

--- netfilter/ip_nat_helper.c
+++ netfilter/ip_nat_helper.c
@@ -26,13 +26,13 @@
 #define ASSERT_READ_LOCK(x) MUST_BE_READ_LOCKED(&ip_nat_lock)
 #define ASSERT_WRITE_LOCK(x) MUST_BE_WRITE_LOCKED(&ip_nat_lock)
 
-#include <linux/netfilter_ipv4/ip_conntrack.h>
-#include <linux/netfilter_ipv4/ip_conntrack_helper.h>
-#include <linux/netfilter_ipv4/ip_nat.h>
-#include <linux/netfilter_ipv4/ip_nat_protocol.h>
-#include <linux/netfilter_ipv4/ip_nat_core.h>
-#include <linux/netfilter_ipv4/ip_nat_helper.h>
-#include <linux/netfilter_ipv4/listhelp.h>
+#include "../include/linux/netfilter_ipv4/ip_conntrack.h"
+#include "../include/linux/netfilter_ipv4/ip_conntrack_helper.h"
+#include "../include/linux/netfilter_ipv4/ip_nat.h"
+#include "../include/linux/netfilter_ipv4/ip_nat_protocol.h"
+#include "../include/linux/netfilter_ipv4/ip_nat_core.h"
+#include "../include/linux/netfilter_ipv4/ip_nat_helper.h"
+#include "../include/linux/netfilter_ipv4/listhelp.h"
 
 #if 0
 #define DEBUGP printk
