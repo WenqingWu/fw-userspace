83f3feada71cb450cab3c8c5b0cd3b1cbd2bc998
WenqingWu <wenqingwu2015@gmail.com>
2018-07-25 15:16:30 +0800

    modified main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-//#include <stdio.h>
+#include <stdio.h>
 //#include <stdlib.h>
 //#include <string.h>
 //#include <stddef.h>
 //#include <sys/socket.h>
 
-//#include "include/linux/skbuff.h"
+#include "include/linux/netdevice.h"
+#include "include/linux/skbuff.h"
 #include "include/linux/netfilter.h"
 #include "include/linux/netfilter_ipv4.h"
 
@@ -15,43 +16,42 @@
 int okfn_sample(struct sk_buff *skb)
 {
     /* processing */
-    //printf("-------------this is okfn.----------\n");
+    printf("-------------this is okfn.----------\n");
     return 1;
 }
 
 int main()
 {
-//   struct sk_buff skb = {0,0};
-//    struct sk_buff *skb = (struct sk_buff *)malloc(sizeof(struct sk_buff));
-//    struct net_device *dev1;
-//   struct net_device *dev2;
+    struct sk_buff skbuff_tmp = {0, 0};
+    struct sk_buff *skbuff = &skbuff_tmp;
+    struct net_device *dev1;
+    struct net_device *dev2;
     int ret;
-//   char *packet = "0x6a8d01e7080045003e234500008011";
     
-//    memcpy(skb, packet, sizeof(struct sk_buff));
+//    memcpy(skbuff, packet, sizeof(struct sk_buff));
 
     /*call NF_IP_PRE_ROUTING, just like done in ip_input.c*/
-    ret = NF_HOOK(PF_INET, NF_IP_PRE_ROUTING, NULL, NULL, NULL,
+    ret = NF_HOOK(PF_INET, NF_IP_PRE_ROUTING, skbuff, NULL, NULL,
 		       okfn_sample);
 
 #if 0
     /*call NF_IP_LOCAL_IN, just like done in ip_input.c*/
-    ret = NF_HOOK(PF_INET, NF_IP_LOCAL_IN, skb, skb->dev, NULL,
+    ret = NF_HOOK(PF_INET, NF_IP_LOCAL_IN, skbuff, skbuff->dev, NULL,
 		       okfn_sample);
 
 
     /*call NF_IP_FORWARD, just like done in ip_forward.c*/
-    ret = NF_HOOK(PF_INET, NF_IP_FORWARD, skb, skb->dev, dev2,
+    ret = NF_HOOK(PF_INET, NF_IP_FORWARD, skbuff, skbuff->dev, dev2,
 		       okfn_sample);
 
 
     /*call NF_IP_LOCAL_OUT, just like done in ip_output.c*/
-    ret = NF_HOOK(PF_INET, NF_IP_LOCAL_OUT, skb, NULL, dev2
+    ret = NF_HOOK(PF_INET, NF_IP_LOCAL_OUT, skbuff, NULL, dev2
 		       okfn_sample);
 
 
     /*call NF_IP_POST_ROUTING, just like done in ip_output.c*/
-    ret = NF_HOOK(PF_INET, NF_IP_POST_ROUTING, skb, NULL, dev2,
+    ret = NF_HOOK(PF_INET, NF_IP_POST_ROUTING, skbuff, NULL, dev2,
 		       okfn_sample);
 
 #endif
